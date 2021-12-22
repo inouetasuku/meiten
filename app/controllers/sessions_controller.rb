bb35d5ccbf509cf8dfb2bb927e48e84bff3ab3d5
inouetasuku <all.laugh.away@gmail.com>
2021-12-22 12:05:05 +0900

    second heroku

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
 
     if user&.authenticate(session_params[:password])
       session[:user_id] = user.id
-      redirect_to root_url, notice: 'ログインしました'
+      redirect_to users_path, notice: 'ログインしました'
     else
       render :new
     end
