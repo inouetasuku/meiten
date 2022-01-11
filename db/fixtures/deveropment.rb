723ca93f9ade9024281a0e59f65cd1fbf25255eb
inouetasuku <all.laugh.away@gmail.com>
2022-01-11 09:35:09 +0900

    seed-fu実装
    ユーザーを１〜１１まで実装

---
+++ db/fixtures/deveropment.rb
@@ -0,0 +1,186 @@
+User.seed do |s|
+  s.id = 1
+  s.email = "meitenmae@sample.com"
+  s.password = "password"
+  s.name = "明転のプロ"
+  s.heat = 1
+  s.location = 3
+  s.location_detail = "東京"
+  s.sex = 1
+  s.age = 45
+  s.hobby = "現場の空気を明るくできます"
+  s.special_skill = "嫌なことを言って暗くすることもできます"
+  s.office = "SMA"
+  s.history_art = 12
+  s.self_introduction = "照明だけでなく空気も明暗使い分けます。よろしくお願いします"
+end
+
+User.seed do |s|
+  s.id = 2
+  s.email = "meimei@sample.com"
+  s.password = "password"
+  s.name = "めいめい"
+  s.heat = 2
+  s.location = 1
+  s.location_detail = "札幌"
+  s.sex = 2
+  s.age = 18
+  s.hobby = "カフェ巡り"
+  s.special_skill = "歌に合わせてタンバリンを奏でます"
+  s.office = "なし"
+  s.history_art = 
+  s.self_introduction = "若さで勝負！よろしくです！！"
+end
+
+User.seed do |s|
+  s.id = 3
+  s.email = "admin@sample.com"
+  s.password = "password"
+  s.name = "アドミン田中"
+  s.heat = 1
+  s.location = 1
+  s.location_detail = "札幌"
+  s.sex = 1
+  s.age = 30
+  s.hobby = "バイト"
+  s.special_skill = "シフト調整"
+  s.office = "吉本"
+  s.history_art = 10
+  s.self_introduction = "あなたをプロデュースして一流芸能人にします！"
+end
+
+User.seed do |s|
+  s.id = 4
+  s.email = "gagaga@sample.com"
+  s.password = "password"
+  s.name = "ガガガ"
+  s.heat = 1
+  s.location = 3
+  s.location_detail = "千葉"
+  s.sex = 1
+  s.age = 22
+  s.hobby = "歌を聞くこと（ロック）"
+  s.special_skill = "セリフをいただいたら、セリフを言いながら雰囲気に合ったギター演奏"
+  s.office = "太田プロダクション"
+  s.history_art = 4
+  s.self_introduction = "リズムネタならお任せ！歌の上手い人を募集"
+end
+
+User.seed do |s|
+  s.id = 5
+  s.email = "kokomi@sample.com"
+  s.password = "password"
+  s.name = "KOKOMI"
+  s.heat = 2
+  s.location = 2
+  s.location_detail = "青森"
+  s.sex = 2
+  s.age = 29
+  s.hobby = "クラブに行くこと"
+  s.special_skill = "コンテンポラリーダンス"
+  s.office = "なし"
+  s.history_art = 
+  s.self_introduction = "歌ネタできる人募集。横でコンテンポラリーダンスします"
+end
+
+User.seed do |s|
+  s.id = 6
+  s.email = "omoro@sample.com"
+  s.password = "password"
+  s.name = "オモロー伯爵"
+  s.heat = 1
+  s.location = 4
+  s.location_detail = "岐阜"
+  s.sex = 1
+  s.age = 33
+  s.hobby = "人々の笑顔を見ること"
+  s.special_skill = "お笑い芸人なら、どんな無名な人でもコンビ名がわかる"
+  s.office = "SMA"
+  s.history_art = 6
+  s.self_introduction = "私と一緒にお客様を笑顔にしようぞ！"
+end
+
+User.seed do |s|
+  s.id = 7
+  s.email = "maki@sample.com"
+  s.password = "password"
+  s.name = "まき"
+  s.heat = 2
+  s.location = 4
+  s.location_detail = "新潟"
+  s.sex = 2
+  s.age = 20
+  s.hobby = "インスタ、tiktok"
+  s.special_skill = "盛れる写真を撮ること"
+  s.office = "なし"
+  s.history_art = 
+  s.self_introduction = "体目当てNG。健全に相方探してます"
+end
+
+User.seed do |s|
+  s.id = 8
+  s.email = "sasutena@sample.com"
+  s.password = "password"
+  s.name = "サステナブル辰吉"
+  s.heat = 1
+  s.location = 5
+  s.location_detail = "大阪"
+  s.sex = 1
+  s.age = 24
+  s.hobby = "ボランティア"
+  s.special_skill = "かつて流行したギャグを新しくする"
+  s.office = "吉本"
+  s.history_art = 1
+  s.self_introduction = "漫才がやりたいです。よろしくお願いします"
+end
+
+User.seed do |s|
+  s.id = 9
+  s.email = "nokogiri@sample.com"
+  s.password = "password"
+  s.name = "ノコギリ"
+  s.heat = 1
+  s.location = 4
+  s.location_detail = "奈良"
+  s.sex = 1
+  s.age = 30
+  s.hobby = "工具集め、工具鑑賞"
+  s.special_skill = "DIY"
+  s.office = "太田プロダクション"
+  s.history_art = 2
+  s.self_introduction = "元大工で芸人に転身しました。DIYの技術を活かしてコント道具を作ります。よろしくお願いします"
+end
+
+User.seed do |s|
+  s.id = 10
+  s.email = "mayoke@sample.com"
+  s.password = "password"
+  s.name = "魔除けの神田"
+  s.heat = 1
+  s.location = 5
+  s.location_detail = "広島"
+  s.sex = 1
+  s.age = 46
+  s.hobby = "アイドルの追っかけ"
+  s.special_skill = "祈祷"
+  s.office = "なし"
+  s.history_art = 
+  s.self_introduction = "普段は一人で活動しています。賞レースに出てみたいので相方募集"
+end
+
+User.seed do |s|
+  s.id = 11
+  s.email = "poporo@sample.com"
+  s.password = "password"
+  s.name = "ポポロクロイス"
+  s.heat = 1
+  s.location = 2
+  s.location_detail = "秋田"
+  s.sex = 1
+  s.age = 31
+  s.hobby = "見たことない景色を見ること"
+  s.special_skill = "写真撮影（写真家としても活動しています）"
+  s.office = "吉本"
+  s.history_art = 5
+  s.self_introduction = "宣材写真を自分で撮ることができます！写真家活動と並行でお笑いをやりたいです"
+end
